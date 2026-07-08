b3e501dbf690b1233ef71081299277ed9ff2214b
am-craftnote <292026240+am-craftnote@users.noreply.github.com>
2026-07-08 19:52:39 +0900

    ハンバーガーメニュー実装

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
 'use strict';
 
 {
+  // Hamburger Menu
+  const menuOpen = document.querySelector('#menu-open');
+  const overlay = document.querySelector('.overlay');
+  const menuClose = document.querySelector('#menu-close');
+  const menuItem = document.querySelectorAll('.overlay ul li a');
+
+  menuOpen.addEventListener('click', () => {
+    overlay.classList.add('show');
+    menuOpen.classList.add('hide');
+  });
+
+  menuClose.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    menuOpen.classList.remove('hide');
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      overlay.classList.remove('show');
+      menuOpen.classList.remove('hide')
+    });
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      overlay.classList.remove('show');
+      menuOpen.classList.remove('hide');
+
+      mask.classList.add('hidden');
+
+      document.querySelectorAll('.gallery-modal').forEach(modal => {
+        modal.classList.add('hidden');
+      });
+
+      document.querySelectorAll('.popular-spot-modal').forEach(modal => {
+        modal.classList.add('hidden');
+      });
+
+      contactModal.classList.add('hidden');
+    }
+  });
+
   // mask共通
   const mask = document.querySelector('#mask');
 
@@ -51,7 +93,7 @@
     });
   });
 
-  
+
   // NEWS
   const tabsButton = document.querySelectorAll('.news-tabs li button');
   const newsContents = document.querySelectorAll('.news-container .content')
